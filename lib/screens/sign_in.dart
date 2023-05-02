7c4ba7ffadbe1e830a0e004c2a730af4cb79dba8
Adarsh <adarshkumar167@gmail.com>
2023-05-02 10:16:54 +0530

    Done

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -12,108 +12,158 @@ class _SignInState extends State<SignIn> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(231, 231, 231, 1),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.1,
-          ),
-          Container(
-            height: MediaQuery.of(context).size.height * 0.4,
-            alignment: Alignment.center,
-            child: Image.asset(
-              'assets/images/signIn.png',
-            ),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.1,
-          ),
-          Container(
-            height: 250,
-            width: MediaQuery.of(context).size.width * 0.9,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.vertical(
-                  top: Radius.elliptical(300, 100),
-                  bottom: Radius.elliptical(300, 100)),
-              color: Colors.white,
-            ),
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 10,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                alignment: Alignment.bottomCenter,
+                height: MediaQuery.of(context).size.height * 0.1,
+                child: Image.asset(
+                  'assets/images/topSlider.png',
+                  width: MediaQuery.of(context).size.width * 0.2,
                 ),
-                const Text(
-                  'Sign In',
-                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 24),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.4,
+                alignment: Alignment.center,
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.1,
+                    ),
+                    Image.asset(
+                      'assets/images/signIn.png',
+                      width: MediaQuery.of(context).size.width * 0.9,
+                    ),
+                  ],
                 ),
-                const SizedBox(
-                  height: 10,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.1,
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.38,
+                width: MediaQuery.of(context).size.width * 0.9,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.vertical(
+                      top: Radius.elliptical(400, 100),
+                      bottom: Radius.elliptical(400, 100)),
+                  color: Colors.white,
                 ),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                      horizontal: MediaQuery.of(context).size.width * 0.1),
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * 0.06,
-                    width: 250,
-                    decoration: BoxDecoration(
-                        color: const Color.fromRGBO(37, 43, 57, 1)
-                            .withOpacity(0.1),
-                        borderRadius: BorderRadius.circular(1000)),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Email/Phone',
-                        fillColor: Colors.grey[200],
-                        filled: true,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.02,
+                    ),
+                    Text(
+                      'Sign In',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: MediaQuery.of(context).size.height * 0.03),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.02,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: MediaQuery.of(context).size.width * 0.1),
+                      child: SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.08,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                            hintText: 'Email/Phone',
+                            fillColor: Colors.grey[200],
+                            filled: true,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                      horizontal: MediaQuery.of(context).size.width * 0.1),
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * 0.06,
-                    width: 250,
-                    decoration: BoxDecoration(
-                        color: const Color.fromRGBO(37, 43, 57, 1)
-                            .withOpacity(0.1),
-                        borderRadius: BorderRadius.circular(1000)),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Password',
-                        fillColor: Colors.grey[200],
-                        filled: true,
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.01,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: MediaQuery.of(context).size.width * 0.1),
+                      child: SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.08,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(100)),
+                            hintText: 'Password',
+                            fillColor: Colors.grey[200],
+                            filled: true,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.05,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(100),
-                    color: const Color.fromRGBO(37, 43, 57, 1).withOpacity(0.1),
-                  ),
-                  child: const Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        'Sign In',
-                        style: TextStyle(fontSize: 24),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.01,
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.05,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: const Color.fromRGBO(37, 43, 57, 1)
+                            .withOpacity(0.1),
                       ),
-                      Icon(Icons.arrow_forward)
-                    ],
-                  ),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.03,
+                            ),
+                            const Text(
+                              'Sign In',
+                              style: TextStyle(fontSize: 24),
+                            ),
+                            Container(
+                              width: MediaQuery.of(context).size.height * 0.05,
+                              height: MediaQuery.of(context).size.height * 0.05,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(100),
+                                color: const Color.fromRGBO(37, 43, 57, 1)
+                                    .withOpacity(0.1),
+                              ),
+                              child: const Icon(Icons.arrow_forward),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.01,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Forgot Password?',
+                            style: TextStyle(color: Colors.grey[500])),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          'New User? Sign Up',
+                          style: TextStyle(color: Colors.grey[500]),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
